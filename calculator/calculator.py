a311fc8778fd9512c0767e6652f54d30fb32cedd
Satyabandi20 <satyaa6644@gmail.com>
2025-02-10 10:58:08 +0000

    Refactored calculator module using OOP principles

--- calculator/calculator.py
+++ calculator/calculator.py
@@ -1,21 +1,15 @@
 """
-Basic Calculator Module with four arithmetic operations.
+Refactored Calculator Module using OOP.
 """
 
-def add(x: float, y: float) -> float:
-    """Returns the sum of two numbers."""
-    return x + y
+from calculator.calculation import Calculation
 
-def subtract(x: float, y: float) -> float:
-    """Returns the difference of two numbers."""
-    return x - y
-
-def multiply(x: float, y: float) -> float:
-    """Returns the product of two numbers."""
-    return x * y
-
-def divide(x: float, y: float) -> float:
-    """Returns the quotient of two numbers. Raises ValueError if division by zero occurs."""
-    if y == 0:
-        raise ValueError("Cannot divide by zero!")
-    return x / y
+class Calculator:
+    """Performs calculations using Calculation class."""
+    @staticmethod
+    def perform_operation(x: float, y: float, operation):
+        """
+        Creates a Calculation object and returns the result.
+        """
+        calculation = Calculation(x, y, operation)
+        return calculation.get_result()
